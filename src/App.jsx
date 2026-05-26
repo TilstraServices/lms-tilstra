ba074373d3fa9ea77fcfd530109ea5ead20e6ef0
Jonathan Roose <jonathan@tilstraservices.nl>
2026-05-26 13:53:29 +0200

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Competentiematrix from "./pages/competentiematrix/Competentiematrix";
+import Beheer from "./pages/competentiematrix/Beheer";
 import "./index.css";
 
 function App() {
   const params = new URLSearchParams(window.location.search);
   const rol = params.get("role") || "trainee";
 
-  return <Competentiematrix rol={rol} />;
+  return (
+    <BrowserRouter basename="/lms-tilstra">
+      <Routes>
+        <Route path="/" element={<Competentiematrix rol={rol} />} />
+        <Route path="/beheer" element={<Beheer />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
