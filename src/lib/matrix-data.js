647afab980f05679671fea8398265ef225dec9dd
Jonathan Roose <jonathan@tilstraservices.nl>
2026-05-26 10:50:39 +0200

    Mappenstructuur en matrix-data toegevoegd

---
+++ src/lib/matrix-data.js
@@ -0,0 +1,124 @@
+export const MATRIX = [
+  {
+    competentie: "Communiceren",
+    indicatoren: [
+      { id: "com-1", naam: "Relatiegericht / Netwerken" },
+      { id: "com-2", naam: "Inlevingsvermogen / Schriftelijk communiceren" },
+      { id: "com-3", naam: "Toegankelijkheid / Helpen" },
+      { id: "com-4", naam: "Overtuigkracht / Mondeling communiceren" },
+    ],
+  },
+  {
+    competentie: "Innovatief vermogen",
+    indicatoren: [
+      { id: "inn-1", naam: "Toekomstgericht" },
+      { id: "inn-2", naam: "Creativiteit / Creëren" },
+    ],
+  },
+  {
+    competentie: "Ondernemend vermogen",
+    indicatoren: [
+      { id: "ond-1", naam: "Proactief / Initiatief nemen" },
+      { id: "ond-2", naam: "Profielerend vermogen" },
+      { id: "ond-3", naam: "Doorzettingsvermogen / Volhouden" },
+      { id: "ond-4", naam: "Omgevingsgerichtheid / Opmerken" },
+    ],
+  },
+  {
+    competentie: "Ontwikkelingsgericht handelen",
+    indicatoren: [
+      { id: "ont-1", naam: "Reflecterend vermogen" },
+      {
+        id: "ont-2",
+        naam: "Zelfsturend vermogen / Zelfsturing en ontwikkeling",
+      },
+    ],
+  },
+  {
+    competentie: "Organiserend vermogen",
+    indicatoren: [
+      { id: "org-1", naam: "Doelgerichtheid / Plannen & Organiseren" },
+      { id: "org-2", naam: "Procesgerichtheid / Controleren" },
+      { id: "org-3", naam: "Oplossingsgericht" },
+      {
+        id: "org-4",
+        naam: "Stressbestendigheid / Functioneren onder spanning",
+      },
+    ],
+  },
+  {
+    competentie: "Samenwerken",
+    indicatoren: [
+      { id: "sam-1", naam: "Taakgerichtheid" },
+      { id: "sam-2", naam: "Mensgerichtheid" },
+      { id: "sam-3", naam: "Aanpassingsvermogen / Flexibel reageren" },
+      { id: "sam-4", naam: "Leiderschap / Leiding geven" },
+    ],
+  },
+  {
+    competentie: "Probleemoplossend vermogen",
+    indicatoren: [
+      { id: "pro-1", naam: "Onderzoekend vermogen" },
+      { id: "pro-2", naam: "Analytisch vermogen / Analyseren" },
+      { id: "pro-3", naam: "Methodisch handelen" },
+      { id: "pro-4", naam: "Conceptueel vermogen" },
+      { id: "pro-5", naam: "Oordeelsvorming / Beslissen" },
+    ],
+  },
+];
+
+export const RADAR_LABELS = [
+  "Relatiegericht",
+  "Inlevingsvermogen",
+  "Toegankelijkheid",
+  "Overtuigkracht",
+  "Toekomstgericht",
+  "Creativiteit",
+  "Proactief",
+  "Profilerend",
+  "Doorzetting",
+  "Omgevingsgericht",
+  "Reflecterend",
+  "Zelfsturend",
+  "Doelgerichtheid",
+  "Procesgerichtheid",
+  "Oplossingsgericht",
+  "Stressbestendig",
+  "Taakgericht",
+  "Mensgericht",
+  "Aanpassingsvermogen",
+  "Leiderschap",
+  "Onderzoekend",
+  "Analytisch",
+  "Methodisch",
+  "Conceptueel",
+  "Oordeelsvorming",
+];
+
+export const RADAR_IDS = [
+  "com-1",
+  "com-2",
+  "com-3",
+  "com-4",
+  "inn-1",
+  "inn-2",
+  "ond-1",
+  "ond-2",
+  "ond-3",
+  "ond-4",
+  "ont-1",
+  "ont-2",
+  "org-1",
+  "org-2",
+  "org-3",
+  "org-4",
+  "sam-1",
+  "sam-2",
+  "sam-3",
+  "sam-4",
+  "pro-1",
+  "pro-2",
+  "pro-3",
+  "pro-4",
+  "pro-5",
+];
