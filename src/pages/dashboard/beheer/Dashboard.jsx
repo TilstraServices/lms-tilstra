4dea53d879e3be9104498695404807d40d8725a0
Jonathan Roose <jonathan@tilstraservices.nl>
2026-06-29 16:56:20 +0200

    Update Dashboard.jsx

--- src/pages/dashboard/beheer/Dashboard.jsx
+++ src/pages/dashboard/beheer/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashboardLayout from "../DashboardLayout";
 import ModulesBoom from "./blokken/ModulesBoom";
 import BeheerHomeBlok from "./blokken/BeheerHomeBlok";
 import GebruikersBeheerBlok from "./blokken/GebruikersBeheerBlok";
+import { HouseIcoon } from "../../../assets/icons.jsx";
 
 export default function BeheerDashboard() {
   const [email, setEmail] = useState(() => localStorage.getItem("email"));
@@ -39,17 +40,7 @@ export default function BeheerDashboard() {
   const navigatie = [
     {
       label: "Home",
-      icoon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          fill="currentColor"
-          viewBox="0 0 256 256"
-        >
-          <path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H53.39a8,8,0,0,0,7.23-4.57,48,48,0,0,1,86.76,0,8,8,0,0,0,7.23,4.57H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40ZM80,144a24,24,0,1,1,24,24A24,24,0,0,1,80,144Zm136,56H159.43a64.39,64.39,0,0,0-28.83-26.16,40,40,0,1,0-53.2,0A64.39,64.39,0,0,0,48.57,200H40V56H216ZM56,96V80a8,8,0,0,1,8-8H192a8,8,0,0,1,8,8v96a8,8,0,0,1-8,8H176a8,8,0,0,1,0-16h8V88H72v8a8,8,0,0,1-16,0Z" />
-        </svg>
-      ),
+      icoon: <HouseIcoon />,
       blok: <BeheerHomeBlok email={email} />,
       sectie: "Beheer",
     },
